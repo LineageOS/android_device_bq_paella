05c3ef5a3f5e3af6bdef452d9f9b79adad9e1e19
Darkmet98 <darkmet98@gmail.com>
2017-01-06 16:56:14 +0100

    Updated device to nougat and LineageOS from https://github.com/X5DEV/android_device_bq_paella Thanks to @marado and @dimusa6
    Change-Id: I4109e776be43cca8596bb23c327027ed7657a703

--- sensors/bst_fifo/version/version.c
+++ sensors/bst_fifo/version/version.c
@@ -5,7 +5,7 @@
 
 static const char daemon_version_str[] = "3.0.39";
 static const char hal_version_str[] = "3.0.17";
-static const char date_str[] = "2015-08-05 17:14:05";
+static const char date_str[] = "2017-01-06 13:04:08";
 const char* get_daemon_version(void)
 {
 	return daemon_version_str;
