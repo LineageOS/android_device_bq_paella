03d6b6a8c24a536f1a6d4e29559972532d3900b2
Darkmet98 <darkmet98@gmail.com>
2017-01-06 17:48:11 +0100

    cmhw: Updated to Nougat
    Change-Id: I23a295f549ee2b5494efe55d00f6b6e1ea07a732

--- cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
+++ cmhw/src/org/cyanogenmod/hardware/KeyDisabler.java
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2016 The CyanogenMod Project
  *
+ * Copyright (C) 2017 The LineageOS Project
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +19,7 @@
 package org.cyanogenmod.hardware;
 
 import java.io.File;
-import org.cyanogenmod.hardware.util.FileUtils;
+import org.cyanogenmod.internal.util.FileUtils;
 
 /*
  * Disable capacitive keys
