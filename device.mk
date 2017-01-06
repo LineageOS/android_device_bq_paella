4e1385c949fa5db5f2df4fa3c7232292e73a9ab2
Darkmet98 <darkmet98@gmail.com>
2017-01-06 17:28:22 +0100

    Add Gello to the PRODUCT_PACKAGES
    Change-Id: I931be0382223bcace91afa5adf0e520f11bae56b

--- device.mk
+++ device.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -141,7 +142,8 @@ PRODUCT_PACKAGES += \
 
 #Apps
 PRODUCT_PACKAGES += \
-	Camera2
+	Camera2 \
+	Gello
 
 # Connectivity Engine support
 PRODUCT_PACKAGES += \
