39a459f41b887d805e378c25c4fe0dae0d9db2be
Marcos Marado <mindboosternoori@gmail.com>
2017-01-13 20:46:11 +0000

    Rebrand to Lineage
    Change-Id: I0d30d9c11618f956f664710f75a1a30f36a5fcaa

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,5 @@
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 LineageOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +21,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
 
-PRODUCT_NAME := cm_paella
+PRODUCT_NAME := lineage_paella
 BOARD_VENDOR := bq
 PRODUCT_DEVICE := paella
 
