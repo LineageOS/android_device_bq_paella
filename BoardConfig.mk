ec7dcdda7e4ce3e8a75f3f3f2db250e833146fca
Kra1o5 <kra1o5x@gmail.com>
2017-01-29 17:17:38 +0100

    paella: N bringup
    Change-Id: I335283a9a0ea93a6a1be58557f9a5c0707eb2f84

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,99 +1,47 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-FORCE_32_BIT := true
+-include device/bq/msm8916-common/BoardConfigCommon.mk
 
--include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+# Platform
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
 
-DEVICE_PATH := device/bq/paella
-TARGET_VENDOR := bq
-
-BOARD_USES_QCOM_HARDWARE := true
-
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
-
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/bq/msm8939
 TARGET_KERNEL_CONFIG := paella_defconfig
-TARGET_USES_UNCOMPRESSED_KERNEL := false
-KERNEL_DEFCONFIG := $(TARGET_KERNEL_CONFIG)
 
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+# Assert
+TARGET_OTA_ASSERT_DEVICE := piccolometal,picmt,paella,Aquaris_X5
 
-# Support form WiFi security modes EAP-SIM, PEAP and AKA
-CONFIG_EAP_PROXY := qmi
-CONFIG_EAP_PROXY_DUAL_SIM := true
-
-# Camera
--include $(DEVICE_PATH)/camera/CameraConfig.mk
-
-#Audio
-BOARD_USES_GENERIC_AUDIO := true
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/bq/paella/bluetooth
 
 # CMHW
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw/src
-
-# DEXPREOPT
-WITH_DEXPREOPT := false
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
-
-# CNE
-BOARD_USES_QCNE := true
-
-# CPU
-TARGET_CPU_CORTEX_A53 := true
-
-# GPS
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-USE_DEVICE_SPECIFIC_GPS := true
-TARGET_NO_RPC := true
-
-# init
-TARGET_LIBINIT_MSM8916_DEFINES_FILE := $(DEVICE_PATH)/init/init_paella.cpp
-
-# liblights
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
-BOARD_PERSISTIMAGE_PARTITION_SIZE := 33552384
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 13100366848
-
-# double tap to wake
+BOARD_HARDWARE_CLASS += device/bq/paella/cmhw/src
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/SMWP"
 
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+# Filesystem
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12895622922 # (BOARD_USERDATAIMAGE_PARTITION_SIZE - 16384 for crypto footer)
 
-# Video
-TARGET_HAVE_SIGNED_VENUS_FW := true
+# Properties
+TARGET_SYSTEM_PROP += device/bq/paella/system.prop
 
-#Encrypt
-TARGET_HW_DISK_ENCRYPTION := false
+# SELinux
+BOARD_SEPOLICY_DIRS += device/bq/paella/sepolicy
 
-BOARD_SEPOLICY_DIRS += \
-    device/bq/paella/sepolicy
-
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
-
-TARGET_KERNEL_SOURCE := kernel/bq/msm8916
-
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
-# inherit from the proprietary version
+# Inherit from the proprietary version
 -include vendor/bq/paella/BoardConfigVendor.mk
--- lineage.mk
+++ lineage.mk
@@ -1,32 +1,22 @@
-# Copyright (C) 2014 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
-$(call inherit-product, device/bq/paella/full_paella.mk)
-
-# Inherit some common Lineage stuff.
+# Inherit some common Lineage stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Must define platform variant before including any common things
-TARGET_BOARD_PLATFORM_VARIANT := msm8916
+# Inherit device configuration
+$(call inherit-product, device/bq/paella/full_paella.mk)
 
+PRODUCT_RELEASE_NAME := Aquaris X5
 PRODUCT_NAME := lineage_paella
-BOARD_VENDOR := bq
-PRODUCT_DEVICE := paella
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   TARGET_DEVICE=Aquaris_X5 PRODUCT_NAME=Aquaris_X5
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=bq/Aquaris_X5/Aquaris_X5:6.0.1/MMB29M/1479371811:user/release-keys \
+    PRIVATE_BUILD_DESC="picmt-user 6.0.1 MMB29M 4.0.0.08_20161117-0936 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-bq
-
-TARGET_VENDOR_PRODUCT_NAME := Aquaris_X5
-TARGET_VENDOR_DEVICE_NAME := Aquaris_X5
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=Aquaris_X5 PRODUCT_NAME=Aquaris_X5
