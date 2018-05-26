418765ab185dc2dabfcc85e5b72ba846a2d04ca7
Michael Bestas <mkbestas@lineageos.org>
2018-05-26 12:26:00 +0000

    paella: Move device specific properties to vendor_prop.mk
    Change-Id: I8649131ad1e8000065944cac0a1f46623011c3e7

---
+++ vendor_prop.mk
@@ -0,0 +1,12 @@
+# Cabl
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qualcomm.cabl=0
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320 \
+    ro.opengles.version=196608
+
+# Perfd
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min_freq_0=800000
